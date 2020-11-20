672a1f41ebb80405e84c06e5277564d8e8c1ef5a
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2020-11-20 17:34:30 +0900

    Initial Commit

---
+++ ScrollViewReaderTest/ScrollViewReaderTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewReaderTestApp.swift
+//  ScrollViewReaderTest
+//
+//  Created by Yusuke Hasegawa on 2020/11/20.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewReaderTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
